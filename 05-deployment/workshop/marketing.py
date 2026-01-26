125670306b331a120b66c29e368fe429677829b8
Lautaro <157620600+lautarocolli@users.noreply.github.com>
2026-01-26 02:29:04 +0000

    developed my first FastAPI app for churn prediction

---
+++ 05-deployment/workshop/marketing.py
@@ -0,0 +1,4 @@
+if churn >= 0.5:
+    print(f'Churn: YES (prob of churning: {churn:.3f})')
+else:
+    print(f'Churn: NO (prob of churning: {churn:.3f})')
--- 05-deployment/workshop/predict.py
+++ 05-deployment/workshop/predict.py
@@ -1,33 +1,26 @@
 import pickle
+import uvicorn
+
+from typing import Dict, Any
+from fastapi import FastAPI
+
+app = FastAPI(title = 'churn-prediction')
 
 with open('model.bin', 'rb') as f_in:
     pipeline = pickle.load(f_in)
 
-customer = {
-    'gender': 'male',
-    'seniorcitizen': 0,
-    'partner': 'no',
-    'dependents': 'yes',
-    'phoneservice': 'no',
-    'multiplelines': 'no_phone_service',
-    'internetservice': 'dsl',
-    'onlinesecurity': 'no',
-    'onlinebackup': 'yes',
-    'deviceprotection': 'no',
-    'techsupport': 'no',
-    'streamingtv': 'no',
-    'streamingmovies': 'no',
-    'contract': 'month-to-month',
-    'paperlessbilling': 'yes',
-    'paymentmethod': 'electronic_check',
-    'tenure': 6,
-    'monthlycharges': 29.85,
-    'totalcharges': 129.85
-}
 
-churn = pipeline.predict_proba([customer])[:, 1]
+def predict_single(customer):
+    churn_proba = pipeline.predict_proba([customer])[0,1]
+    return churn_proba
 
-if churn >= 0.5:
-    print(f'Churn: YES (prob of churning: {churn[0]:.3f})')
-else:
-    print(f'Churn: NO (prob of churning: {churn[0]:.3f})')
+@app.post("/predict")
+def predict(customer: Dict[str, Any]):
+    churn_proba = predict_single(customer)
+    return {
+        "churn_probability": churn_proba,
+        "churn": bool(churn_proba >= 0.5)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run("predict:app", host="0.0.0.0", port=8080, reload=True)
